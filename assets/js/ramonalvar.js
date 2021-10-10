5f5d07848bb742ec28a94017ffa02561f7fa3de0
ibarrond <alberto.ibarrondo-luis@eurecom.fr>
2021-10-10 12:13:21 +0200

    Gallery + Filter

--- assets/js/ramonalvar.js
+++ assets/js/ramonalvar.js
@@ -1,9 +1,17 @@
 
-
-$('.grid').isotope({
-    itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
-    percentPosition: true,
-    masonry: {
-      columnWidth: '.grid-sizer'
-    }
-  });
+$(window).load(function () {
+    $('.grid').isotope({
+        itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+        percentPosition: true,
+        resizesContainer: true,
+        resizable: true,
+        masonry: {
+        columnWidth: '.grid-sizer'
+        }
+    });
+    // filter items on button click
+    $('.btn-group').on( 'click', 'button', function() {
+        var filterValue = $(this).attr('data-filter');
+        $('.grid').isotope({ filter: filterValue });
+    });
+});
