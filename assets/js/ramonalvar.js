03ac944589bdee67f961ec708496f4f294d043e9
ibarrond <alberto.ibarrondo-luis@eurecom.fr>
2021-10-11 00:30:52 +0200

    Filter from url hashtag

--- assets/js/ramonalvar.js
+++ assets/js/ramonalvar.js
@@ -1,5 +1,6 @@
 
 $(window).load(function () {
+    // ISOTOPE: Masonry layout, filtering and sorting
     $('.grid').isotope({
         itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
         percentPosition: true,
@@ -9,9 +10,41 @@ $(window).load(function () {
         columnWidth: '.grid-sizer'
         }
     });
-    // filter items on button click
-    $('.btn-group').on( 'click', 'button', function() {
+    // ISOTOPE FILTERS: filter items on button click
+    var $filters = $('.btn-group').on( 'click', 'button', function() {
         var filterValue = $(this).attr('data-filter');
         $('.grid').isotope({ filter: filterValue });
+        // set filter in hash
+        location.hash = 'filter=' + encodeURIComponent( filterValue );
     });
+    var isIsotopeInit = false;
+    
+    // ISOTOPE FILTER URL HASHTAG: filter items on button click
+
+    function getHashFilter() {
+        var hash = location.hash;
+        // get filter=filterName
+        var matches = location.hash.match( /filter=([^&]+)/i );
+        var hashFilter = matches && matches[1];
+        return hashFilter && decodeURIComponent( hashFilter );
+    }
+
+    function onHashchange() {
+      var hashFilter = getHashFilter();
+      if ( !hashFilter && isIsotopeInit ) {
+        return;
+      }
+      isIsotopeInit = true;
+      // filter isotope
+      $('.grid').isotope({filter: hashFilter});
+      // set selected class on button
+      if ( hashFilter ) {
+        $filters.find('.is-checked').removeClass('is-checked');
+        $filters.find('[data-filter="' + hashFilter + '"]').addClass('is-checked');
+      }
+    }
+    
+    $(window).on( 'hashchange', onHashchange );
+    // trigger event handler to init Isotope
+    onHashchange();
 });
